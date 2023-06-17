5cacf04e28654bda826485c579393060f2eaaee1
Ross Bermudez <rbermudez@anaconda.com>
2023-06-17 22:18:49 +0200

    fix cypress tests

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -1,17 +1,12 @@
-describe('sample test', () => {
-  beforeEach(() => {
-    cy.visit('/')
-  })
+describe('Check layout', () => {
+    beforeEach(() => {
+        cy.visit('/');
+    });
 
-  it('displays the resources text', () => {
-    cy.get('h1')
-    .contains('Welcome to my app!');
-  })
-  it('renders the Netlify logo image', () => {
-    cy.get('img')
-    .should('be.visible')
-    .and(($img) => {
-      expect($img[0].naturalWidth).to.be.greaterThan(0);
-    })
-  })
-})
+    it('displays the website name', () => {
+        cy.get('h1').contains('Ross & Raquel');
+    });
+    it('renders the background', () => {
+        cy.get('.hero.bg-banner').should('be.visible').should('have.css', 'background-image');
+    });
+});
