4227acb975e84c36f13c8fff623154c6b5996b0b
Ross Bermudez <rbermudez@anaconda.com>
2023-09-22 12:14:43 +0200

    chore(deps): update @vercel-og

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -1,41 +1,49 @@
-// @ts-nocheck
 import { ImageResponse } from '@vercel/og';
+import Image from 'next/image';
 
 export const runtime = 'edge';
 
 const image = fetch(new URL('./image.jpg', import.meta.url)).then((res) => res.arrayBuffer());
 
 export async function GET() {
-    const imageData = await image;
-    return new ImageResponse(
-        (
-            <div
-                style={{
-                    display: 'flex',
-                    height: '100%',
-                    width: '100%',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    flexDirection: 'column',
-                    fontSize: 60,
-                    letterSpacing: -2,
-                    fontWeight: 700,
-                    textAlign: 'center',
-                    color: 'rgb(39, 59, 58)',
-                }}>
-                <img
-                    style={{ position: 'absolute', width: '100%', height: '100%' }}
-                    alt={'Ross & Raquel'}
-                    src={imageData}
-                />
-                <div>Ross</div>
-                <div>&</div>
-                <div>Raquel</div>
-            </div>
-        ),
-        {
-            width: 1200,
-            height: 630,
-        }
-    );
+    try {
+        const imageData: any = await image;
+
+        return new ImageResponse(
+            (
+                <div
+                    style={{
+                        display: 'flex',
+                        height: '100%',
+                        width: '100%',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        flexDirection: 'column',
+                        fontSize: 60,
+                        letterSpacing: -2,
+                        fontWeight: 700,
+                        textAlign: 'center',
+                        color: 'rgb(39, 59, 58)',
+                    }}>
+                    <Image
+                        style={{ position: 'absolute', width: '100%', height: '100%' }}
+                        alt={'Ross & Raquel'}
+                        src={imageData}
+                    />
+                    <div>Ross</div>
+                    <div>&</div>
+                    <div>Raquel</div>
+                </div>
+            ),
+            {
+                width: 1200,
+                height: 630,
+            }
+        );
+    } catch (e: any) {
+        console.log(`${e.message}`);
+        return new Response(`Failed to generate the image`, {
+            status: 500,
+        });
+    }
 }
